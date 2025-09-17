b22f0fba52371ff6a6b46eb932c9e724b9453cf6
AtharvaParadkar <atharvaparadkar91@gmail.com>
2025-09-17 22:37:29 +0530

    Add api_service.dart for API interaction

---
+++ lib/api_service.dart
@@ -0,0 +1,74 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+import 'package:oktoast/oktoast.dart';
+
+class ApiService {
+
+  final String baseUrl = "http://10.0.2.2:5000/api";    /// for android emulator
+  // final String baseUrl = "http://localhost:5000/api";    /// for iOS simulator
+  // final String baseUrl = "http://10.54.64.146:5000/api";    /// for real device
+
+  final storage = FlutterSecureStorage();
+
+  Future register (String username, String password) async {
+    final response = await http.post(
+      Uri.parse("$baseUrl/auth/register"),
+      headers: {"content/type" : "application/json"},
+      body: jsonEncode({"username" : username, "password" : password})
+    );
+
+    if (response.statusCode == 200) {
+      log("Register response body ${response.body}");
+      return showToast("Register Successful!!");
+    } else {
+      log("Error ${response.body}");
+      return showToast("error in register");
+    }
+  }
+
+  Future login (String username, String password) async {
+    final response = await http.post(
+      Uri.parse("$baseUrl/auth/login"),
+      headers: {"content/type" : "application/json"},
+      body: jsonEncode({"username" : username, "password" : password})
+    );
+
+    if (response.statusCode == 200) {
+      log("Login response body ${response.body}");
+      return showToast("Login Successful!!");
+    } else {
+      log("Error ${response.body}");
+      return showToast("error in login");
+    }
+  }
+
+  Future<List<dynamic>> getTodos () async {
+    final token = await storage.read(key: "token");
+    final response = await http.get(
+      Uri.parse("$baseUrl/todos"),
+      headers: {"Authorization" : "Bearer $token"},
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception("Failed to load todos");
+    }
+  }
+
+  Future<void> addTodo (String text) async {
+    final token = await storage.read(key: "token");
+    await http.post(
+      Uri.parse("$baseUrl/todos"),
+      headers: {
+        "Authorization" : "Bearer $token",
+        "content/type" : "application.json",
+      },
+      body: jsonEncode({"text" : text})
+    );
+  }
+
+}
