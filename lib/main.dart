4da8c90506e851ad4c61f1db26f338f1a45d2608
AtharvaParadkar <atharvaparadkar91@gmail.com>
2025-09-17 22:02:19 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+void main () {
+  runApp(ToDo());
+}
+
+class ToDo extends StatelessWidget {
+  const ToDo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter + Express",
+    );
+  }
+}
